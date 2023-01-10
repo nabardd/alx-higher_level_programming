38fe7a29a7ff876298e0e85532c09d27ac2f0f3e
nabardd <ojebiyidaviddaniel@gmail.com>
2023-01-10 03:23:09 +0100

    Update 0-add.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 def print_argv(argv):
-  n =  len(argv) - 1
+    n =  len(argv) - 1
 
-  if n == 0:
-    print("{:d} argument.".format(n))
-    return
-  elif n == 1:
-    print("{:d} argument: ".format(n))
-  else:
-    print("{:d} arguments: ".format(n))
+    if n == 0:
+      print("{:d} arguments.".format(n))
+      return
+    elif n == 1:
+      print("{:d} argument: ".format(n))
+    else:
+      print("{:d} arguments: ".format(n))
 
-  i = 1
-  
-  while i <= n:
-    print("{:d}: {:s}".format(i, argv[i]))
+    i = 1
+    
+    while i <= n:
+      print("{:d}: {:s}".format(i, argv[i]))
 
-    i += 1
+      i += 1
 
 if __name__ == "__main__":
-  import sys
-  print_argv(sys.argv)
+    import sys
+    print_argv(sys.argv)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def sum_args(argv):
-  n = len(argv) - 1
-  if n == 0:
-    print("{:d}".format(n))
-  else:
-    sum = 0
-    for i in range(1, len(argv) + 1):
-      sum += int(argv[i])
+    n = len(argv) - 1
+    if n == 0:
+      print("{:d}".format(n))
+    else:
+      sum = 0
+      for i in range(1, len(argv) + 1):
+        sum += int(argv[i])
 
-    print("{:d}".format(sum))
+      print("{:d}".format(sum))
 
 if __name__ == "__main__":
-  import sys
-  sum_args(sys.argv)
+    import sys
+    sum_args(sys.argv)
