df2561f9049dd45fcdc6a8ddfa8a7118b806df5b
nabardd <ojebiyidaviddaniel@gmail.com>
2023-01-25 12:04:48 +0100

    Update 0x06 files

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -89,6 +89,6 @@ class Square:
         else:
             [print("") for y in range(self.__position[1])]
             for i in range(self.__size):
-                [print(" ", end="") for k in range(self.__position[0])]
+                [print("", end="") for k in range(self.__position[0])]
                 [print("#", end="") for n in range(self.__size)]
                 print("")
