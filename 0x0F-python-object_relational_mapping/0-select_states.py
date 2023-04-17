fa243f0751dc17f0140ce8712bf8dbd89dfecf88
nabardd <ojebiyidaviddaniel@gmail.com>
2023-04-17 11:03:32 +0100

    Add 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_usa,
+the script accepts three arguments:
+	-- mysql username,
+	-- mysql password,
+	-- database name
+"""
+
+import sys
+import MySQLdb
+
+
+def db_connect():
+	try:
+		db_connection = MySQLdb.connect(
+        	user=sys.argv[1],
+        	password=sys.argv[2],
+        	database=sys.argv[3],
+		)
+	except Exception as e:
+		print(f"Could not connect to the database: {e}")
+		return 0
+
+	print("Connected")
+
+	cursor = db_connection.cursor()
+	cursor.execute("SELECT * FROM `states` ORDER BY id;")
+
+	results = cursor.fetchall()
+
+	for idx, state in enumerate(results):
+		print((idx, state))
+
+
+if __name__ == "__main__":
+	db_connect()
