c3688cef7f2629a7318f77a047f1c4763419f81b
nabardd <ojebiyidaviddaniel@gmail.com>
2023-04-27 12:25:41 +0100

    Add all 0x10

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Takes a URL and displays all the HTTP methods the
+# server will accept.
+
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Takes in a URL as an argument, sends a GET request
+# and displays the body of the URL
+
+curl -sH "X-School-User-Id: 98" "$1"
